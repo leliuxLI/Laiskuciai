a70ca63658f1e55717de88dfb6c106b48a490592
leliuxLI <64489616+leliuxLI@users.noreply.github.com>
2022-04-11 20:26:19 +0300

    Created Project

--- viewer/views.py
+++ viewer/views.py
@@ -2,7 +2,9 @@ import re
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import FormView, ListView
-from django.forms import (CharField, DateField, Form, IntegerField, ModelChoiceField, Textarea)
+from django.forms import (CharField, DateField, EmailInput, Form, IntegerField, ModelChoiceField, PasswordInput, Textarea)
+import smtplib # biblioteka susikalbėjimui su pašto serveriu
+from email.message import EmailMessage
 
 
 # Create your views here.
@@ -19,14 +21,39 @@ class EmailViews(Form):
   email_from = CharField(max_length=128)
   email_to = CharField(max_length=128)
   email_subject = CharField(max_length=128)
-  email_text = CharField(max_length=1024, widget=Textarea)
+  email_text = CharField(max_length=1024)
+  sending_email = CharField(max_length=128)
+  sending_pasword = CharField(max_length=128, widget=PasswordInput)
 
-  def clean_email_text(self):
-    # Force each sentence of the description to be capitalized.
-    initial = self.cleaned_data['email_text']
-    sentences = re.sub(r'\s*\.\s*', '.', initial).split('.')
-    return '. '.join(sentence.capitalize() for sentence in sentences)
 
+
+  def send_email(self, email_from, email_to, email_subject, email_text, sending_email, sending_pasword ):
+    email = EmailMessage()
+    email['from'] = email_from
+    email['to'] = email_to
+    email['subject'] = email_subject
+
+    email.set_content(email_text)
+
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo() # žiūrėkite, kaip į pasisveikinimą su serveriu
+        smtp.starttls() # inicijuojame šifruotą kanalą
+        smtp.login(sending_email, sending_pasword) # nurodome prisijungimo duomenis
+        smtp.send_message(email) # išsiunčiame žinutę
+
+  
 class EmailCreateView(FormView):
   template_name = 'emailform.html'
   form_class = EmailViews
+  success_url = '/thanks/'
+
+  def form_valid(self, form):
+    # This method is called when valid form data has been POSTed.
+    # It should return an HttpResponse.
+    email_from = form.data['email_from']
+    email_to = form.data['email_to']
+    email_subject = form.data['email_subject']
+    email_text = form.data['email_text']
+    
+    form.send_email(email_from, email_to, email_subject, email_text)
+    return super().form_valid(form)
