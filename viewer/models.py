f12b7fc7f2fc1406596fe167bfc8686b113ba464
leliuxLI <64489616+leliuxLI@users.noreply.github.com>
2022-04-23 21:00:25 +0300

    versija2

--- viewer/models.py
+++ viewer/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 class EmailSetings(models.Model):
     smtp_host = models.CharField(max_length=200)
@@ -16,12 +16,23 @@ class MainTemplate(models.Model):
     email_to = models.EmailField(max_length=128)
     email_subject = models.CharField(max_length=128)
     email_text = models.TextField()
-    week_monday = models.BooleanField()
+    email_start = models.DateTimeField(auto_now_add=True, blank=True)
+    email_end = models.DateTimeField(auto_now_add=True, blank=True)
+    week_monday = models.BooleanField(default=False)
+    week_tuesday = models.BooleanField(default=False)
+    week_wednesday = models.BooleanField(default=False)
+    week_thursday = models.BooleanField(default=False)
+    week_friday = models.BooleanField(default=False)
+    week_saturday = models.BooleanField(default=False)
+    week_sunday = models.BooleanField(default=False)
     server_list = models.ForeignKey(EmailSetings, on_delete=models.CASCADE)    
 
 class UserList(models.Model):
     user_name = models.CharField(max_length=200)
     user_email = models.CharField(max_length=200)
     template_list = models.ForeignKey(MainTemplate, on_delete=models.CASCADE)
+
+
+
 
 
--- viewer/views.py
+++ viewer/views.py
@@ -1,4 +1,7 @@
 #from curses.textpad import Textbox
+from calendar import week
+from dataclasses import dataclass
+import datetime
 import re
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -21,7 +24,15 @@ class EmailTemplates(Form):
   email_to = CharField(max_length=128, widget=EmailInput)
   email_subject = CharField(max_length=128)
   email_text = CharField(max_length=10000, widget=Textarea)
+  email_start = DateField()
+  email_end = DateField()
   week_monday = BooleanField()
+  week_tuesday = BooleanField()
+  week_wednesday = BooleanField()
+  week_thursday = BooleanField()
+  week_friday = BooleanField()
+  week_saturday = BooleanField()
+  week_sunday = BooleanField() 
   server_list = ModelChoiceField(queryset=EmailSetings.objects.all()) 
     
 
@@ -96,13 +107,24 @@ class TemplateCreateView(FormView):
     _email_to = form.data['email_to']
     _email_subject = form.data['email_subject']
     _email_text = form.data['email_text']
+    _email_start = form.data['email_start']
+    _email_end = form.data['email_end']
     _week_monday = form.data['week_monday']
+    _week_tuesday = form.data['week_tuesday']
+    _week_wednesday = form.data['week_wednesday']
+    _week_thursday = form.data['week_thursday']
+    _week_friday = form.data['week_friday']
+    _week_saturday = form.data['week_saturday']
+    _week_sunday = form.data['week_sunday']
     _server_list = EmailSetings.objects.get(id=int(form.data['server_list']))
- 
-    if _week_monday == "on":
-      _week_monday = True
-    else:
-      _week_monday = False
+
+    _week = [_week_monday, _week_tuesday,_week_wednesday,_week_thursday,_week_friday, _week_saturday,_week_sunday]
+    j = -1
+    for i in _week:
+      j += 1
+      if i == "on": _week[j] = True
+      else: _week[j] = False
+
     
   
     server_item = MainTemplate(template_name=_email_templ,
@@ -110,9 +132,35 @@ class TemplateCreateView(FormView):
                                email_to=_email_to,
                                email_subject=_email_subject,
                                email_text=_email_text,
-                               week_monday=_week_monday,
+                               email_start = _email_start,
+                               email_end = _email_end,
+                               week_monday= _week[0],
+                               week_tuesday = _week[1],
+                               week_wednesday = _week[2],
+                               week_thursday = _week[3],
+                               week_friday = _week[4],
+                               week_saturday = _week[5],
+                               week_sunday = _week[6],
                                server_list=_server_list
                                )
     server_item.save()
+    
+    # import sched, time
+    # s = sched.scheduler(time.time, time.sleep)
+    # def print_time(a='default'):
+    #       print("From print_time", time.time(), a)
 
+    # def print_some_times():
+    #   while True:
+    #     print(time.time())
+    #     s.enter(10, 1, print_time)
+    #     s.enter(10, 1, time.sleep(1))
+    #     s.enter(5, 2, print_time, argument=('positional',))
+    #     s.enter(5, 1, print_time, kwargs={'a': 'keyword'})
+        
+    #     s.run(blocking=False)
+    
+    #     print(time.time())
+ 
+    # print_some_times()
     return super().form_valid(form)
